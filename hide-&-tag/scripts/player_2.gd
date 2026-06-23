c461a3a0186393caea07f1d47c8625214b9863e5
jatinsacodes <jatinschool20@gmail.com>
2026-06-23 19:37:47 +1200

    Player movement
    Players can move now

---
+++ hide-&-tag/scripts/player_2.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_key_pressed(KEY_UP) and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get input direction.
+	var direction := 0.0
+	if Input.is_key_pressed(KEY_LEFT):
+		direction -= 1.0
+	if Input.is_key_pressed(KEY_RIGHT):
+		direction += 1.0
+
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if Input.is_key_pressed(KEY_DOWN):
+		position.y += 1
+
+	move_and_slide()
